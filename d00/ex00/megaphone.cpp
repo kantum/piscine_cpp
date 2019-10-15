915d17fd4f735b6089c0c0636094967627f939bc
Your Name <you@example.com>
2019-10-15 10:53:24 +0200

    ex00 finish

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
 #include <string>
 
-void	ft_print(std::string str)
-{
-	std::cout << str;
-}
-
-void	ft_printnl(std::string str)
-{
-	std::cout << str << std::endl;
-}
-
 std::string	ft_str_toupper(std::string str)
 {
 	std::string ret;
 
 	for (unsigned int i = 0; i < str.length(); i++)
-	{
 		ret += static_cast<char>(toupper(str[i]));
-	}
 	return ret;
 }
 
@@ -27,11 +15,9 @@ int	main(int argc, char** argv)
 	if (argc > 1)
 	{
 		for (int i = 1; i < argc; i++)
-			ft_print(ft_str_toupper(argv[i]));
+			std::cout << ft_str_toupper(argv[i]);
 		std::cout << std::endl;
 	}
 	else
-	{
-		ft_printnl("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
-	}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
